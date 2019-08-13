d8271ddfe247ca8a28239de37aa28296bc33f9f5
Eric Yan <github@ericyan.me>
2019-08-13 22:19:41 +0200

    Add a basic volume indicator

--- main.go
+++ main.go
@@ -2,10 +2,21 @@ package main
 
 import (
 	"barista.run"
+	"barista.run/bar"
 	"barista.run/modules/clock"
+	"barista.run/modules/volume"
+	"barista.run/outputs"
 )
 
 func main() {
+	barista.Add(volume.DefaultSink().Output(func(vol volume.Volume) bar.Output {
+		if vol.Mute {
+			return outputs.Text("Vol: Muted")
+		}
+
+		return outputs.Textf("Vol: %d%%", vol.Pct())
+	}))
+
 	barista.Add(clock.Local().OutputFormat("Mon 15:04"))
 
 	barista.Run()
