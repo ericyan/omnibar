510018a315093c9a0c7552b0b4ce43618526dd9b
Eric Yan <github@ericyan.me>
2019-08-14 22:41:02 +0200

    Use custom click handler for volume indicator

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os/exec"
+
 	"barista.run"
 	"barista.run/bar"
 	"barista.run/modules/clock"
@@ -10,11 +12,25 @@ import (
 
 func main() {
 	barista.Add(volume.DefaultSink().Output(func(vol volume.Volume) bar.Output {
+		var seg *bar.Segment
 		if vol.Mute {
-			return outputs.Text("Vol: Muted")
+			seg = outputs.Text("Vol: Muted")
+		} else {
+			seg = outputs.Textf("Vol: %d%%", vol.Pct())
 		}
 
-		return outputs.Textf("Vol: %d%%", vol.Pct())
+		return seg.OnClick(func(e bar.Event) {
+			switch e.Button {
+			case bar.ButtonLeft:
+				vol.SetMuted(!vol.Mute)
+			case bar.ButtonRight:
+				exec.Command("gnome-control-center", "sound").Run()
+			case bar.ScrollUp:
+				vol.SetVolume(vol.Vol + (vol.Max-vol.Min)/100)
+			case bar.ScrollDown:
+				vol.SetVolume(vol.Vol - (vol.Max-vol.Min)/100)
+			}
+		})
 	}))
 
 	barista.Add(clock.Local().OutputFormat("Mon 15:04"))
